606b275946c5544fbeaed738546057849ddf6b10
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-07-25 12:13:16 -0300

    Como fucnionam as Rotas

--- flask/app.py
+++ flask/app.py
@@ -8,5 +8,16 @@ app = Flask(__name__)
 def home():
     return "Bem-vindo ao seu primeiro app de Flask"
 
+# rotas
+
+@app.route("/api/data")
+def api_data():
+    data = {"nome": "Flask", "versao": "3.1"}
+    return jsonify(data)
+
+@app.route("/user/<username>")
+def user_profile(username):
+    return f"Perfil do usuario: {username}"
+
 if __name__ == "__main__":
     app.run(debug=True)
